c96119243b8c3bcc37d22ff1bced79af51f01172
Marcelo916 <161655728+Marcelo916@users.noreply.github.com>
2024-04-12 13:30:58 -0700

    Login activity is working as expected when I enter a valid username and password. We still need to implement the database.

--- app/src/main/java/com/example/arithmeticforkids/LoginActivity.java
+++ app/src/main/java/com/example/arithmeticforkids/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,10 +24,29 @@ public class LoginActivity extends AppCompatActivity {
         binding.loggingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), 0);
-                startActivity(intent);
+                if(!verifyUser()) {
+                    Toast.makeText(LoginActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), 0);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    //We will have to update this method. The code is to test the login page working properly.
+    private boolean verifyUser() {
+        //Hardcoded valid user and password
+        String validUsername = "example_user";
+        String validPassword = "password123";
+
+        // Get the entered username and password from Text fields
+        String enteredUsername = binding.userName.getText().toString().trim();
+        String enteredPassword = binding.password.getText().toString().trim();
+
+        // Check if the entered username and password match the valid credentials
+        return enteredUsername.equals(validUsername) && enteredPassword.equals(validPassword);
+
     }
 
     static Intent loginIntentFactory(Context context) {
